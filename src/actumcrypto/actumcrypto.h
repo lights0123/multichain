fdca68bb0295f11b7d9e8a3eb8b90ab8e2e5c7ba
codegreen <ryansshane@rocketmail.com>
2018-04-05 18:35:41 -0400

    Create actumcrypto.h

---
+++ src/actumcrypto/actumcrypto.h
@@ -0,0 +1,10 @@
+#ifndef ACTUMCRYPTO_H
+
+#include "entities/asset.h"
+#include "protocol/multichainscript.h"
+
+#include <boost/assign/list_of.hpp>
+
+void RewardMinedBlock(CWallet pwallet, int amount);
+
+#endif ACTUMCRYPTO_H
