454ca802363ffb8b1f939070536564bd36043a55
lights0123 <me@lights0123.com>
2018-04-06 01:33:17 -0400

    Add CWallet

--- src/actumcrypto/actumcrypto.h
+++ src/actumcrypto/actumcrypto.h
@@ -1,10 +1,13 @@
 #ifndef ACTUMCRYPTO_H
+#define ACTUMCRYPTO_H
 
 #include "entities/asset.h"
 #include "protocol/multichainscript.h"
 
 #include <boost/assign/list_of.hpp>
 
+class CWallet;
+
 void RewardMinedBlock(CWallet* pwallet, int amount);
 
-#endif ACTUMCRYPTO_H
+#endif // ACTUMCRYPTO_H
