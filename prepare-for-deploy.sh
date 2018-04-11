f3b088c430740e5d2b09a707a9a7af4b5d439752
Ben Schattinger <lights0123@users.noreply.github.com>
2018-04-10 22:13:13 -0400

    Fix deployment script

--- prepare-for-deploy.sh
+++ prepare-for-deploy.sh
@@ -2,4 +2,4 @@
 
 #Run as: ./prepare-for-deploy.sh platformName versionNumber
 
-tar -czf "actumcrypto-multichain-$2-$3.tar.gz" actumcrypto/*
+tar -czf "actumcrypto-multichain-$1-$2.tar.gz" actumcrypto/*
